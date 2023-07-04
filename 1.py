60adcf8ad941d212134c51a45591dcc3d3701ae9
sandeepa t n <saandeepatalagini@gmail.com>
2023-07-04 07:56:02 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,26 @@
+
+print("mce\n"*3,end="")
+name=input("What is your name ").strip().title()
+
+print(f"how are you, {name}")
+name,y=name.split(" ")
+print(name)
+print(y)
+match name:
+    case "Sandy":
+        print("IS")
+    case "Kris":
+        print("CSE")
+    case _:
+        print("no")    
+
+for _ in range(3):
+    print("mce")
+while True:
+        try:
+
+            n=int(input("enter number "))
+            if n>0:
+                break    
+        except:
+            print("n is not a integer")     
---
+++ calculator.py
@@ -0,0 +1,4 @@
+x=int(input("Enter the first operator"))
+#op=(input("enter the second operator"))
+y=int(float(input("Enter the first operator")))
+print(f"{x+y:,}")
